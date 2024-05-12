03d36f88941ba56d1f04c3822d61e0b152ebf680
Angga Prasetia Mulia <164855799+anggaprasetiamulia@users.noreply.github.com>
2024-05-12 22:59:00 +0700

    Tugas Komputasi Matematika

--- run_numerical_differentiation.m
+++ run_numerical_differentiation.m
@@ -1,4 +1,4 @@
-f = @(x) x.^3 - 3*x.^2 + 2*x - 1;
+f = @(x) x.^2 - 2*x.^2 + 2*x.^2 - 1;
 [df_forward, df_backward, df_central] = numerical_differentiation(f, 2, 0.1);
 fprintf('Turunan menggunakan Beda Maju: %f\n', df_forward);
 fprintf('Turunan menggunakan Beda Mundur: %f\n', df_backward);
